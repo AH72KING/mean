cf4eb81bc1ef6a5d627cd50c8486b7c202479e0a
Ahsan Dev <ahsandev.creative@gmail.com>
2017-11-21 12:43:27 +0500

    User Add to Cart only once

--- mean-relational/packages/system/public/controllers/header.controller.js
+++ mean-relational/packages/system/public/controllers/header.controller.js
@@ -10,6 +10,23 @@ angular
         /*$scope.UploadUrl = 'http://localhost:3000/products/assets/';*/
         $rootScope.UploadUrl            = UploadUrl;   
         //check key if expire, then logout user
+        // validate key
+        $scope.validateKey = function(){
+          var currentUId  =  Session.getItem('UserID');
+          if(typeof currentUId != 'undefined' && currentUId != null){
+            var key   =  Session.getItem('key_'+currentUId);
+            var url = baseUrl+'api/validateKey';
+            var postData = {'key':key};
+            var configObj = { method: 'POST',url: url, data: postData, headers: headers};
+            $http(configObj)
+                .then(function onFulfilled(response) {
+                    if(response.data != true){
+                      logout();
+                    }
+                }).catch( function onRejection(errorResponse) {
+                }); 
+          }
+        };
         $scope.validateKey();
         $scope.defaultAvatar = 'http://localhost:3000/products/assets/images/default-avatar.png';
         var vm = this;
@@ -119,23 +136,5 @@ angular
           }
         };
 
-
-        // validate key
-        $scope.validateKey= function(){
-          var currentUId  =  Session.getItem('UserID');
-          if(typeof currentUId != 'undefined' && currentUId != null){
-            var key   =  Session.getItem('key_'+currentUId);
-            var url = baseUrl+'api/validateKey';
-            var postData = {'key':key};
-            var configObj = { method: 'POST',url: url, data: postData, headers: headers};
-            $http(configObj)
-                .then(function onFulfilled(response) {
-                    if(response.data != true){
-                      logout();
-                    }
-                }).catch( function onRejection(errorResponse) {
-                }); 
-          }
-        }
     }
 })();
