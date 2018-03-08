5df8103e22e8b6c6fd34c593ddc1a38752dd3566
EC2 Default User <ec2-user@ip-172-31-36-201.us-west-2.compute.internal>
2018-03-08 11:19:38 +0000

    all is well

--- mean-relational/config/env/all.js
+++ mean-relational/config/env/all.js
@@ -6,7 +6,7 @@ module.exports = {
 	port: process.env.PORT || 3000,
     forceSequelizeSync: process.env.FORCE_DB_SYNC === 'true',
     enableSequelizeLog: true,
-    timezone : '-05:00' ,
+    timezone : '+05:00' ,
     aggregate:true,
     expressSessionSecret: '$u234r$ecr13t$e$$ionKey123$$para$amto$2' // replace with your own
 };
